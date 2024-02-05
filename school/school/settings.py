b25601db046a2ac3bd8149fd10536f2dd6f92569
anupama <anupamaunni612@gmail.com>
2024-02-05 14:15:46 +0530

    latest base

--- school/school/settings.py
+++ school/school/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 # settings.py
 
-ALLOWED_HOSTS = ['your_domain.com', 'school-ms.onrender.com', 'other_allowed_domain.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'school-ms.onrender.com', 'other_allowed_domain.com']
 
 
 
